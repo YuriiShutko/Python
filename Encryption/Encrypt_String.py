04601d442eed8c26fe46a4c48ace52a14f2d060f
Yuriy Shutko <yurayurban72@gmail.com>
2019-12-24 14:27:34 +0200

    Check mode moved to main method

--- Encryption/Encrypt_String.py
+++ Encryption/Encrypt_String.py
@@ -4,24 +4,20 @@ import string
 def main():
     input_file = input('Input file name: ')
     output_file = input('Output file name: ')
-
+    key = int(input('Input key: '))
+    mode = input('1 - encrypt\n2 - decrypt\nMake your choice: ')
+    if mode == '2':
+        key = -key
+    elif mode != '1':
+        print("Illegal parameter...")
+        exit()
     with open(input_file, 'r') as r:
         input_str = r.read()
     with open(output_file, 'w') as w:
-        w.write(encrypt_string(input_str, int(input('Input number shift: ')),
-                               input('1 - encrypt\n2 - decrypt\nMake your choice: ')))
-        w.flush()
+        w.write(encrypt_string(input_str, key))
 
 
-def encrypt_string(input_string, number_shift, mode):
-    if mode == '1':
-        number_shift = number_shift
-    elif mode == '2':
-        number_shift = -number_shift
-    else:
-        print("Illegal parameter...")
-        exit()
-
+def encrypt_string(input_string, key):
     a = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     symbols = a + " " + a.upper() + string.ascii_letters + string.digits + string.punctuation
     encrypted_string, length_symbols = "", len(symbols)
@@ -30,7 +26,7 @@ def encrypt_string(input_string, number_shift, mode):
         if found_char_index == -1:
             encrypted_string += input_char
         else:
-            new_index = (found_char_index + number_shift) % length_symbols
+            new_index = (found_char_index + key) % length_symbols
             encrypted_string += symbols[new_index]
 
     return encrypted_string
